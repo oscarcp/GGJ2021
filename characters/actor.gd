66346bdff22900b479f087fffe538d890f8f07b3
Oscar Carballal Prego <oscar.carballal@protonmail.com>
2021-01-28 21:38:14 +0100

    More stuff

--- characters/actor.gd
+++ characters/actor.gd
@@ -1,15 +1,9 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed: = Vector2(100.0, 1000.0)
-export var velocity: = Vector2(100.0, 0)
-export var gravity = 400.0
-export var inertia = 100.0
+const FLOOR_NORMAL = Vector2.UP
 
-func _physics_process(delta: float) -> void:
-	# Add gravity
-	velocity.y += gravity * delta
-	# Prevent infinite acceleration!
-	velocity.y = max(velocity.y, speed.y)
-	# Go!
-	velocity = move_and_slide(velocity)
+# Hmmm... should speed be variable during gameplay as a mechanic?
+export var speed: = Vector2(100.0, 500.0)
+export var gravity = 400.0
+var _velocity: = Vector2.ZERO
---
+++ characters/bear.gd
@@ -0,0 +1,5 @@
+extends "res://characters/actor.gd"
+
+func _physics_process(delta: float) -> void:
+	if is_on_wall():
+		pass
