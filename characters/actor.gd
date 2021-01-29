28f7c90aa1b33afb85f33aec6d91727a7dec7398
Oscar Carballal Prego <oscar.carballal@protonmail.com>
2021-01-29 14:05:23 +0100

    More stuff

--- characters/actor.gd
+++ characters/actor.gd
@@ -4,6 +4,6 @@ class_name Actor
 const FLOOR_NORMAL = Vector2.UP
 
 # Hmmm... should speed be variable during gameplay as a mechanic?
-export var speed: = Vector2(100.0, 500.0)
+export var speed: = Vector2(100.0, 750.0)
 export var gravity = 400.0
 var _velocity: = Vector2.ZERO
--- characters/bear.gd
+++ characters/bear.gd
@@ -1,5 +1,22 @@
 extends "res://characters/actor.gd"
 
+func _ready() -> void:
+    set_physics_process(false)
+    _velocity.x = -speed.x
+
+func _on_PlayerDetector_body_entered(body: Node) -> void:
+    # If player enters area2d go towards him
+    # Careful with physics layers!
+    pass
+
+func _on_WeaponDetector_body_entered(body: Node) -> void:
+    # If torch is active, start menacing movement 10 seconds
+    # If torch not active stop movement and growl for 5 seconds
+    # Release the character
+    queue_free()
+
 func _physics_process(delta: float) -> void:
-	if is_on_wall():
-		pass
+    _velocity.y += gravity * delta
+    if is_on_wall():
+        _velocity.x *= -1.0
+    _velocity.y = move_and_slide(_velocity * 1.3, FLOOR_NORMAL).y
